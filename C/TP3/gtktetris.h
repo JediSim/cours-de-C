bb148f0bd286cef3529e0b7ba4aafe3b966700be
Simon LEONARD <git-1001af4@sinux.sh>
2021-10-09 13:52:51 +0200

    C: TP3 Question 4.2 Dessin de la grille
    C'est une version amélioré de ce qui a été envoyé au prof en soumission
    initiale

---
+++ C/TP3/gtktetris.h
@@ -0,0 +1,41 @@
+#define TAILLE_CARRE 16
+#define MARGE_LARGEUR 4
+#define MARGE_HAUTE 5
+#define MARGE_BASSE 2
+
+typedef struct {
+  Grille g;
+  Piece tab[ NB_PIECES ];
+  Piece piece; // la piece "en cours" que l'on cherche à placer.
+  int col; // la colonne actuelle pour la pièce que l'on cherche à caser.
+  int score; // le score
+  int delay; // le delay
+  GtkWidget* label_score;
+  GtkWidget* label_delay;
+} Jeu;
+
+typedef struct {
+  double r;
+  double g;
+  double b;
+} Color;
+
+gboolean gauche( GtkWidget *widget, gpointer data );
+
+gboolean droite( GtkWidget *widget, gpointer data );
+
+gboolean bas( GtkWidget *widget, gpointer data );
+
+gboolean new( GtkWidget *widget, gpointer data );
+
+Color getColor(char c);
+
+void dessineCarre( cairo_t* cr, int ligne, int colonne, char c);
+
+void dessineGrille( cairo_t* cr, Grille g );
+
+gboolean realize_evt_reaction( GtkWidget *widget, gpointer data );
+
+gboolean on_draw( GtkWidget *widget, GdkEventExpose *event, gpointer data );
+
+void creerIHM( Jeu* j );
