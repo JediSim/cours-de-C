f34947f7d8c75c9b1f8bca4cbf64fdd4518e8e35
JediSim <simpiccc@gmail.com>
2022-01-10 16:42:39 +0100

    init semestre 2

--- algo/arbre.h
+++
@@ -1,9 +0,0 @@
-struct noeud_int
-{
-    int val;
-    struct noeud_int *brg;
-    struct noeud_int *brd;
-};
-
-typedef struct noeud_int *arbre_int;
-
--- projet/cours marty.md
+++
@@ -1,3 +0,0 @@
-# Orga general du projet
-
-- premiere reu il aura pas beaucoup de jeton car on a pas l'infra (un peu de DevOps avant)
