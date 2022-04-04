93aab8c8fe281dca2327d7eff15d68c2286a3df9
JediSim <simpiccc@gmail.com>
2022-04-04 09:56:17 +0200

    TP types abstraits : assert 3
    Co-authored-by: Sinux <sinux-l5d@users.noreply.github.com>

--- Types abstraits/TP/TP1.py
+++ Types abstraits/TP/TP1.py
@@ -71,57 +71,101 @@ def tri_insert_seq(tab, i):
 
 # ===================================================================== tri tas
 
-def swap(tab,i,j):
-    tmp = tab[i]
+def assertion_est_tas(t, p):
+    for i in range(1, p):
+        if 2*i < p and 2*i+1 < p:
+            if t[i] > t[2*i] or t[i] > t[2*i+1]:
+                return False
+    return True    
+
+implique = lambda p, q: not p or q
+# { p = n ET t[1..p] est un tas } => { t[1..n] est un tas }
+def assertion2_tas(t):
+    n = len(t) - 1
+    return implique(
+        t[0] == n and assertion_est_tas(t, t[0]),
+        assertion_est_tas(t,n)
+    )
+
+def assertion3_tas(t):
+    """
+    La fonction assertion3_tas(t) renvoie Vrai si t est un tas et si les éléments de t sont en ordre
+    strictement decroissant
+    
+    :param t: the tableau
+    :return: Vrai ou faux
+    """
+    p = t[0]
+    est_tas = assertion_est_tas(t, p)
+    decroissant = True
+    petit = True
+    for i in range(p+1, len(t)-1):
+        if t[i] < t[i+1]:
+            decroissant = False
+    
+    for i in range(1,p):
+        if t[i] < t[p+1]:
+            petit = False
+
+    return est_tas and decroissant and petit
+    
+
+# Permute les éléments i et j du tab
+def permuter(tab, i, j):
+    temp = tab[i]
     tab[i] = tab[j]
-    tab[j] = tmp
-    return tab
+    tab[j] = temp
 
-def ajout_tas(tab,p,x):
-    p = p+1
-    tab[p] = x
-    i = p
-    while i > 0 and tab[i//2] > tab[i]:
-        tab = swap(tab,i,i//2)
-        i = i//2
-
-def retire_min_tas(tab,p):
-    mini = tab[0]
-    tab[0] = tab[p]
-    # p -= 1
-    i = 0
+# ajoute un élement dans le tas
+def ajouter_tas(t, x):
+    t[0] = t[0] + 1
+    i = t[0]
+    t[i] = x
+    
+    while i>1 and t[int(i/2)] > t[i]: ######## assertion 
+        permuter(t, i, int(i/2))
+        i = int(i/2)
+    
+# retire le racine du tas (i.e. le plus petit élément) et le renvoie, puis réorganise le tas
+def retirer_min(t):
+    min = t[1]
+    t[1] = t[t[0]]
+    t[0] = t[0] - 1
+    i = 1
     fini = False
-    while (i <= (p//2)) and not fini:
-        if p == 2*i or tab[2*i] < tab[2*i+1]:
+
+    while i <= int(t[0]/2) and not fini:
+
+        if t[0] == 2*i or t[2*i] < t[2*i + 1]:
             j = 2*i
         else:
-            j = 2*i+1
+            j = 2*i + 1
         
-        if tab[i] > tab[j]:
-            tab = swap(tab,i,j)
+        if t[i] > t[j]:
+            permuter(t, i, j)
             i = j
         else:
             fini = True
-
-    return mini
-
-
-def tri_tas(tab):
-    p = 0
-    while p < len(tab)-1:
-        ajout_tas(tab,p,tab[p+1])
-        p+=1
-
-    assert p == len(tab)-1
-
-    # p-=1
-    while p > 0:
-        p-=1
-        mini = retire_min_tas(tab,p)
-        tab[p+1] = mini
     
-    return tab
+    return min
 
+# tri le tableau avec l'algorithme du tri par tas.
+def tri_tas(t):
+    t[0] = 1
+
+    while t[0] < len(t)-1:
+        ajouter_tas(t, t[t[0]+1])
+        # { t[1..p] est un tas }
+        assert assertion_est_tas(t, t[0])
+    
+    # { p = n ET t[1..p] est un tas } => { t[1..n] est un tas }
+
+    while t[0] > 1:
+        min = retirer_min(t)
+        t[t[0]+1] = min
+        # { t[1..p] est un tas ET t[p+1..n] est trié dans l'ordre décroissant ET Pour tout e€[1..p], t[e] >= t[p+1] }
+
+    # { p = 1 ET t[1] >= t[2] ET t[2..n] est trié dans l'ordre décroissant } => { t[1..n] est trié dans l'ordre décroissant }
 
 def printNiveau(a,nb,n):
     for _ in range(0,2*n):
@@ -136,14 +180,13 @@ def affiche_arbre(a,i,niveau):
 
 if __name__ == "__main__": 
     # try:
-    tab = [randint(0, 100) for _ in range(10)]
+    # tab = [None]
+    tab = [None] + [randint(0, 100) for _ in range(10)]
     print("tab : ", tab)
     # tab_trie = tri_insert_seq(tab,len(tab)-1)
     # print("tab trié : ", tab_trie)
     # except AssertionError:
     #     print("Aïe! Notre sémantique n'est pas bonne...") # N'arrive jamais
-    tab_tri = tri_tas(tab)
-    print("tab trié : ", tab_tri)
-    affiche_arbre(tab_tri,0,1)
-    print(tab[::-1])
-
+    tri_tas(tab)
+    print("tab trié : ", tab)
+    affiche_arbre(tab,0,1)
