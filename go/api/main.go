441e1cb8f3a1ef07ce32f0fbd6b0a2803a98cd67
JediSim <simpiccc@gmail.com>
2022-07-27 15:23:15 +0200

    go: fin de l'api CRUD tres basique

--- go/api/main.go
+++ go/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"workspace/src/typeApi"
@@ -12,52 +13,109 @@ import (
 
 var Articles []typeApi.Article
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
 }
 
-func returnAllArticles(w http.ResponseWriter, r *http.Request){
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnAllArticles")
 	json.NewEncoder(w).Encode(Articles)
 }
 
 func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    key := vars["id"]
+	vars := mux.Vars(r)
+	key := vars["id"]
 
-    // Loop over all of our Articles
-    // if the article.Id equals the key we pass in
-    // return the article encoded as JSON
-    for _, article := range Articles {
-        if article.Id == key {
-            json.NewEncoder(w).Encode(article)
-        }
-    }
+	// Loop over all of our Articles
+	// if the article.Id equals the key we pass in
+	// return the article encoded as JSON
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
 }
 
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	// get the body of our POST request
+	// unmarshal this into a new Article struct
+	// append this to our Articles array.
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article typeApi.Article
+	json.Unmarshal(reqBody, &article)
+	// update our global Articles array to include
+	// our new Article
+	Articles = append(Articles, article)
+
+	json.NewEncoder(w).Encode(article)
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	// once again, we will need to parse the path parameters
+	vars := mux.Vars(r)
+	// we will need to extract the `id` of the article we
+	// wish to delete
+	id := vars["id"]
+
+	// we then need to loop through all our articles
+	for index, article := range Articles {
+		// if our id path parameter matches one of our
+		// articles
+		if article.Id == id {
+			// updates our Articles array to remove the
+			// article
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+
+}
+
+func updateArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var updatedEvent typeApi.Article
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, &updatedEvent)
+	for i, article := range Articles {
+		if article.Id == id {
+			article.Title = updatedEvent.Title
+			article.Desc = updatedEvent.Desc
+			article.Content = updatedEvent.Content
+			Articles[i] = article
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+
+}
 
 func handleRequests() {
-    // creates a new instance of a mux router
-    myRouter := mux.NewRouter().StrictSlash(true)
-    // replace http.HandleFunc with myRouter.HandleFunc
-    myRouter.HandleFunc("/", homePage)
-    myRouter.HandleFunc("/all", returnAllArticles)
-	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
-    // finally, instead of passing in nil, we want
-    // to pass in our newly created router as the second
-    // argument
-    log.Fatal(http.ListenAndServe(":8080", myRouter))
+	// creates a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+	// replace http.HandleFunc with myRouter.HandleFunc
+	myRouter.HandleFunc("/", homePage).Methods("GET")
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle).Methods("GET")
+	myRouter.HandleFunc("/articles", returnAllArticles).Methods("GET")
+
+	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+
+	myRouter.HandleFunc("/article/{id}", updateArticle).Methods("PUT")
+	// finally, instead of passing in nil, we want
+	// to pass in our newly created router as the second
+	// argument
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func init() {
 	Articles = []typeApi.Article{
-        typeApi.Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-        typeApi.Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
-    }
+		typeApi.Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+		typeApi.Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+	}
 }
 
 func main() {
 	fmt.Printf("Articles: %v\n", Articles)
-    handleRequests()
+	handleRequests()
 }
